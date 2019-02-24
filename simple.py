5d9c6898f504e35acc75a1e3130b738ef9495399
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-02-24 08:03:15 +0200

    Add CLI for configuring the seed, model and result location.

--- simple.py
+++ simple.py
@@ -7,6 +7,8 @@
 
 """Minimal script for generating an image using pre-trained StyleGAN generator."""
 
+import argparse
+import hashlib
 import io
 import os
 import pickle
@@ -16,12 +18,22 @@ import dnnlib
 import dnnlib.tflib as tflib
 import config
 
-def main():
+def main(seed = '', model_name = 'faces', output = config.result_dir):
     # Initialize TensorFlow.
     tflib.init_tf()
 
+    model_options = {
+        'faces': config.model_faces,
+        'celebs': config.model_celebs,
+        'cats': config.model_cats,
+    }
+
+    model = model_options[model_name]
+
+    print('Using model: ' + model_name)
+
     # Load pre-trained network.
-    with open(config.model_faces, 'rb') as f:
+    with open(model, 'rb') as f:
         _G, _D, Gs = pickle.load(f)
         # _G = Instantaneous snapshot of the generator. Mainly useful for resuming a previous training run.
         # _D = Instantaneous snapshot of the discriminator. Mainly useful for resuming a previous training run.
@@ -30,8 +42,17 @@ def main():
     # Print network details.
     Gs.print_layers()
 
+    if seed and len(seed) > 0:
+        str = seed 
+    else:
+        str = ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
+
+    print('Using seed: ' + str)
+
+    seed = int(hashlib.sha256(str.encode('utf-8')).hexdigest(), 16) % 10**8
+
     # Pick latent vector.
-    rnd = np.random.RandomState(5)
+    rnd = np.random.RandomState(seed)
     latents = rnd.randn(1, Gs.input_shape[1])
 
     # Generate image.
@@ -39,9 +60,22 @@ def main():
     images = Gs.run(latents, None, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
 
     # Save image.
+    if output and len(output) > 0:
+        result_dir = output 
+    else:
+        result_dir = config.result_dir
+
     os.makedirs(config.result_dir, exist_ok=True)
-    png_filename = os.path.join(config.result_dir, 'example.png')
+    png_filename = os.path.join(result_dir, str + '_' + model_name + '_' + 'generated.png')
     Image.fromarray(images[0], 'RGB').save(png_filename)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('seed', default='', metavar='Seed', nargs='?', help='Set your seed string. It will use a random seed if unset.')
+    parser.add_argument('-o', '--output', default='', help='The output directory where you want the file.')
+    parser.add_argument('-m', '--model', default='faces', choices=['faces', 'celebs', 'cats'], help='The model that should be used to generate the image.')
+    args = parser.parse_args()
+    
+    print(args)
+
+    main(seed=args.seed, model_name=args.model, output=args.output)
